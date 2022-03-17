e4e02c9d6e5882755c0c7a5dde49f4971c964dd5
nietovaca <nietovaca@gmail.com>
2022-03-17 15:07:34 -0600

    cleaning code

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -29,21 +29,3 @@ const TopNav = (prop)  => {
 }
 
 export default TopNav
-
-// <AppBar position="static" bgimage="">
-//   <Toolbar>
-    // <IconButton
-    //   size="large"
-    //   edge="start"
-    //   color="inherit"
-    //   aria-label="menu"
-    //   sx={{ mr: 2 }}
-    // >
-    //   <MenuIcon />
-    // </IconButton>
-    // <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-    //   Audiophile
-    // </Typography>
-    // <Button color="inherit">Login</Button>
-//   </Toolbar>
-// </AppBar>
