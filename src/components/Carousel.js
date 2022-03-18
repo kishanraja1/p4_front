6f925f2cb5d03c030438194d53b129034af8bb87
nietovaca <nietovaca@gmail.com>
2022-03-18 13:27:53 -0600

    topNav drawer set up

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -81,7 +81,33 @@ const Carousel = (props) => {
     cssEase: "linear",
     // nextArrow: <NextArrow />,
     // prevArrow: <PrevArrow />,
-    beforeChange: (current, next) => setImageIndex(next)
+    beforeChange: (current, next) => setImageIndex(next),
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
   };
 
     return (
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,28 +1,69 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-// import Link from '@mui/material/Link';
+import {useState} from 'react';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+import Drawer from "@mui/material/Drawer";
+import CloseIcon from "@mui/icons-material/Close";
+import Divider from "@mui/material/Divider";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import FolderIcon from "@mui/icons-material/Folder";
+import ImageIcon from "@mui/icons-material/Image";
+import DescriptionIcon from "@mui/icons-material/Description";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
 
 const TopNav = (prop)  => {
+  const [open, setState] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+  if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    return;
+  }
+  setState(open);
+};
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="default" elevation={0} className="TopNav">
+      <AppBar position="static" color="default" elevation={0} className="TopNav" >
+        <Container>
         <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="secondary"
-          aria-label="menu"
-          sx={{ m: 2 }}
+          <IconButton
+            size="large"
+            edge="start"
+            color="secondary"
+            onClick={toggleDrawer(true)}
+            // sx={{
+            //   ml: 1,
+            //   display: {
+            //     // xs: 'block',
+            //     // sm: 'none',
+            //   }
+            // }}
+          >
+            <MenuIcon />
+          </IconButton>
+        <Drawer
+          anchor="left"
+          variant="temporary"
+          open={open}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
         >
-          <MenuIcon />
-        </IconButton>
+            <Box>
+              Inner Content of APP Bar
+            </Box>
+        </Drawer>
         </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
