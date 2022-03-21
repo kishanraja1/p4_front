8ea56d9cafc849623dce32be75dbbc07b8ab13d8
nietovaca <nietovaca@gmail.com>
2022-03-21 11:55:12 -0600

    created & colored add artist dialogue

--- src/components/AddArtist.js
+++ src/components/AddArtist.js
@@ -7,8 +7,11 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 //MUI Icons
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
@@ -51,40 +54,39 @@ const AddArtist = (props) => {
   return(
     <div>
     <AddCircleIcon className="addCircleIcon" aria-label="Add New Music" onClick={handleOpen} color="primary" sx={{ fontSize: 50, padding: 1}}/>
-      <Modal
+      <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="Add Artists Here"
         aria-describedby="Add artists to the database"
-        maxWidth="xs"
+        maxWidth="md"
         className="modal"
+        PaperProps={{sx: {bgcolor: '#ff80ab'}}}
       >
-        <Box component="form" sx={styleModal} onSubmit={handleSubmitArtist}>
-          <Typography variant="h6">
-            Add Artist
-          </Typography>
-          <Typography sx={{ mt: 2 }}>
-            <TextField
-              name="name" onChange={handleChangeArtist}
-              variant="outlined" label="Artist"
-               value={artist.name}
-              sx={{ m: 1, color:'#19196E'}}/>
-            <TextField
-              name="genre" onChange={handleChangeArtist}
-              variant="outlined" label="Genre"
-               value={artist.genre}
-              sx={{ m: 1, color:'#19196E'}}/>
-            <TextField
-              name="image" onChange={handleChangeArtist}
-              variant="outlined" label="Image URL"
-               value={artist.image}
-              sx={{ m: 1, color:'#19196E'}}/>
-          </Typography>
-          <Button variant="outlined" sx={{margin: 2, border: 2, color:'#19196E'}}>
-            <input type="submit"/>
-          </Button>
-        </Box>
-      </Modal>
+          <DialogTitle variant="h6">Add an Artist</DialogTitle>
+          <DialogContent>
+            <Box component="form" onSubmit={handleSubmitArtist}>
+              <TextField
+                name="name" onChange={handleChangeArtist}
+                variant="outlined" label="Artist"
+                 value={artist.name}
+                sx={{ m: 1, color:'#19196E'}}/>
+              <TextField
+                name="genre" onChange={handleChangeArtist}
+                variant="outlined" label="Genre"
+                 value={artist.genre}
+                sx={{ m: 1, color:'#19196E'}}/>
+              <TextField
+                name="image" onChange={handleChangeArtist}
+                variant="outlined" label="Image URL"
+                 value={artist.image}
+                sx={{ m: 1, color:'#19196E'}}/>
+            <Button variant="outlined" sx={{margin: 2, border: 2, color:'#19196E'}}>
+              <input type="submit"/>
+            </Button>
+          </Box>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 
